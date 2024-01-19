a15f1a3bc80a94945f44deea8b735605a08fa21a
huangyy <huangyy@myzr.com.cn>
2024-01-19 16:54:14 +0800

    [edit] conf.py

--- conf.py
+++ conf.py
@@ -129,7 +129,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sphinx.tex', u'唐斌的文档 Documentation',
+    (master_doc, 'sphinx.tex', u'明远智睿的文档',
      u'唐斌', 'manual'),
 ]
 
@@ -139,7 +139,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sphinx', u'唐斌的文档 Documentation',
+    (master_doc, 'sphinx', u'明远智睿的文档',
      [author], 1)
 ]
 
@@ -150,7 +150,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sphinx', u'唐斌的文档 Documentation',
+    (master_doc, 'sphinx', u'明远智睿的文档',
      author, 'sphinx', 'One line description of project.',
      'Miscellaneous'),
 ]
