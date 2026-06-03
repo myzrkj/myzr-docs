62b88ba196116ef12bb3828aedfd31183162afce
huangyy <huangyy@myzr.com.cn>
2026-06-03 08:45:34 +0800

    update for zh/en

--- zh/conf.py
+++ zh/conf.py
@@ -2,7 +2,7 @@
 import os
 
 # 中文配置
-language = 'zh'
+language = 'zh_CN'
 master_doc = 'index'
 
 project = '明远智睿文档'
@@ -11,7 +11,7 @@ author = 'myZR'
 
 extensions = ['sphinx_rtd_theme']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-templates_path = ['_templates']
+templates_path = ['../_templates']
 html_static_path = ['../_static']
 html_theme = 'sphinx_rtd_theme'
 
